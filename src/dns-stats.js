6dd9d44cba8ff1814017cfd588bf6993b6b82012
yelantsevv <yelantsevv@gmail.com>
2024-12-12 23:41:08 +0200

    fix: 4

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const res = {};
+  domains.forEach(el => {
+    const arr = el.split('.');
+    let key = '';
+    for (let i = arr.length - 1; i >= 0; i--) {
+      key += '.' + arr[i];
+      if (!res[key]) {
+        res[key] = 1;
+      } else {
+        res[key]++;
+      }
+    }
+
+
+  })
+  return res;
 }
 
 module.exports = {
