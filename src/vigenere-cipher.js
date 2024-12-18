c1ff6793d8c6b53dbc2a53a4dce0f6daf4eda783
yelantsevv <yelantsevv@gmail.com>
2024-12-18 23:07:02 +0200

    fix: 21

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,59 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  string;
+  keyCopy;
+
+  constructor(type = true) {
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, key) {
+    return this.#process(str, key, true);
+  }
+
+  decrypt(str, key) {
+    return this.#process(str, key, false);
+  }
+  #check(str, key) {
+    if (!str || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+  }
+  #process(str, key, isEncrypt) {
+    this.#check(str, key);
+
+    this.string = str.toUpperCase();
+    this.keyCopy = key.toUpperCase();
+
+    let result = '';
+    let counter = 0;
+
+    for (let i = 0; i < this.string.length; i++) {
+      if (counter >= this.keyCopy.length) {
+        counter = 0;
+      }
+
+      if (this.letters.includes(this.string[i])) {
+        const charIndex = this.letters.indexOf(this.string[i]);
+        const letterIndex = this.letters.indexOf(this.keyCopy[counter]);
+        const offset = charIndex + (isEncrypt ? letterIndex : - letterIndex);
+
+        // Нормализуем индекс и добавляем символ в результат
+        result += this.letters[(offset + this.letters.length) % this.letters.length];
+        counter++;
+      } else {
+        result += this.string[i];
+      }
+    }
+
+    // Возвращаем результат с учетом типа машины (обычная или реверсивная)
+    return this.type ? result : [...result].reverse().join('');
   }
 }
+
+
 
 module.exports = {
   VigenereCipheringMachine
